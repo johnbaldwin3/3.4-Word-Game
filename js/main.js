9874e14dbd96c0b1a417f3945fc181f3b12b5018
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-01-29 23:33:42 -0500

    almost working, need bug fix with duplicated letters

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ var gameInd;
 var guess;
 var gameSpaces = '';
 var winOrLoseDisplay = document.getElementById('win-or-lose');
+var numOrRepeat = document.getElementById('num-or-repeat');
 var gamesWon = document.getElementById('wins');
 var gameWon = 0;
 var gameLost = 0;
@@ -28,6 +29,10 @@ var gameWordList = commonWords.filter(function(word){
     return word;
   }
 });
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "rgba(255, 255, 255, .7)";
+ctx.fillRect(0, 0, myCanvas.height, myCanvas.width);
 // console.log(gameWordList); tested array
 
 //userGuess element hooked to text input box
@@ -62,6 +67,8 @@ console.log(gameWord);
 function runGame(){
   gameWordIndex();
   createGameWord();
+  drawGallows();
+  ropeBeforeHanging();
   for (var i = 0; i < gameWord.length; i++) {
       //set index to increment so it can be actionable later
     gameSpaces =  gameSpaces + '<span id="s'+i+'">'+'_'+"</span>" ;
@@ -72,6 +79,7 @@ function runGame(){
     return gameSpaces;
 }
 runGame();
+
 
 //create function to limit turns and end game
 function turnLimiter() {
@@ -105,12 +113,23 @@ function guessValid() {
   if (guessCC>64 && guessCC<91 || guessCC>96 && guessCC<123) {
     displayGuess();
   } else {
+    numOrRepeat.textContent = "That's a number fool!";
     $('#number-modal').modal('show');
     turnsLeft += 1;
   }
 }
 
+function guessDuplicated() {
+  if ( guessedWord.includes(guess) && dispLet.innerHTML.includes(guess) && guess != Number) {
+    turnsLeft++;
+    numOrRepeat.textContent = "You already tried that one!";
+    $('#number-modal').modal('show');
+  }
+
+}
+
 function guessCheck() {
+  guessValid();
 
   gameWordArray.forEach(function(letter, index)  {
 
@@ -118,22 +137,15 @@ function guessCheck() {
     //  alert("already tried that one");
     //}
     if (letter == guess) {
-      if (dispLet.innerHTML.includes(guess)) {
-        alert("You've already guessed that one!");
-      }
       guessedWord += letter;
       turnsLeft += 1;
-      console.log('letter',letter);
+      console.log('guess', guess);
+      console.log('ltr4',letter);
       console.log('guessedWord', guessedWord);
       //console.log('s'+index);
     var spanSelect = document.getElementById('s'+index);
       //console.log(spanSelect);
       spanSelect.innerHTML = letter;
-
-      // if(gameWord.indexOf(guessedWord) != -1){
-      //   correctGuesses += 1;
-      //
-      //       }
       if (guessedWord.length == gameWord.length) {
         $('#myModal').modal('show');
         gameWon += 1;
@@ -142,7 +154,7 @@ function guessCheck() {
     //  if (correctGuesses == gameWord.length) {
     //    $('#myModal').modal('show');
     //  }
-    }
+  }
 
   });
 
@@ -172,12 +184,13 @@ playAgainButton.addEventListener('click', function(){
 // }
 // })
 
+
 userSubmitButton.addEventListener('click', function(){
   guess = userGuess.value;
   userGuess.value = '';
   guessCheck();
-  guessValid();
   turnLimiter();
+  drawDude();
   //if (guess.length < 1) {
   //  alert("Not a valid guess, homie!");
 //} else {
@@ -189,7 +202,210 @@ userSubmitButton.addEventListener('click', function(){
 })
 
 
+function drawDude() {
+  ///hangman dude....
 
+  if (turnsLeft == '7') {
+    drawHead();
+  } else if (turnsLeft == '6') {
+    drawBody();
+  } else if (turnsLeft == '5') {
+    drawRight();
+  } else if (turnsLeft == '4') {
+    drawLeft();
+  } else if (turnsLeft == '3') {
+    drawRightArm();
+  } else if (turnsLeft == '2') {
+    drawLeftArm();
+  } else if (turnsLeft == '1'){
+    drawHeadGameOver();
+  }
+
+
+}
+
+
+
+function drawGallows(){
+
+
+  //left side of gallows
+  ctx.beginPath();
+  ctx.moveTo(250, 300);
+  ctx.lineTo(250, 30);
+  ctx.stroke();
+
+  //right side of gallows
+  ctx.beginPath();
+  ctx.moveTo(270, 300);
+  ctx.lineTo(270, 15);
+  ctx.stroke();
+
+  //bottom of gallow pole
+  ctx.beginPath();
+  ctx.moveTo(250, 30);
+  ctx.lineTo(100, 30);
+  ctx.stroke();
+
+  //top of gallow pole
+  ctx.beginPath();
+  ctx.moveTo(270, 15);
+  ctx.lineTo(100, 15);
+  ctx.stroke();
+
+  //close of gallow
+  ctx.beginPath();
+  ctx.moveTo(100, 15);
+  ctx.lineTo(100, 30);
+  ctx.stroke();
+
+  //rope loop
+  ctx.beginPath();
+  ctx.arc(150, 123, 18, 0, Math.PI * 2, true);
+  ctx.stroke();
+
+
+
+}
+
+function ropeBeforeHanging() {
+
+  //rope prior to hanging
+  ctx.beginPath();
+  ctx.moveTo(150,30);
+  ctx.lineTo(150,107);
+  ctx.stroke();
+}
+
+function drawHead() {
+
+  //header of dude
+
+  ctx.beginPath();
+  ctx.arc(150, 76, 25, 0, Math.PI * 2, true);
+  ctx.stroke();
+
+  //straight face
+  ctx.beginPath();
+  ctx.moveTo(140, 90);
+  ctx.lineTo(160, 90);
+  ctx.stroke();
+
+  //right eye
+  ctx.beginPath();
+  ctx.moveTo(140, 70);
+  ctx.lineTo(147, 70);
+  ctx.stroke();
+
+  //left eye
+  ctx.beginPath();
+  ctx.moveTo(160, 70);
+  ctx.lineTo(153, 70);
+  ctx.stroke();
+
+
+
+  //rope after hanging
+  ctx.beginPath();
+  ctx.moveTo(150,30);
+  ctx.lineTo(150,50);
+  ctx.stroke();
+
+
+}
+
+function drawBody() {
+//body
+  ctx.beginPath();
+  ctx.moveTo(150,100);
+  ctx.lineTo(150, 200);
+  ctx.stroke();
+
+}
+
+function drawHeadGameOver() {
+
+  //header of dude
+  ctx.fillStyle = "#00F2F2";
+  ctx.beginPath();
+  ctx.arc(150, 76, 25, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.stroke();
+
+  //straight face
+  ctx.beginPath();
+  ctx.moveTo(140, 90);
+  ctx.lineTo(160, 90);
+  ctx.stroke();
+
+  //right eye
+  ctx.beginPath();
+  ctx.moveTo(140, 70);
+  ctx.lineTo(147, 70);
+  ctx.stroke();
+
+  //left eye
+  ctx.beginPath();
+  ctx.moveTo(160, 70);
+  ctx.lineTo(153, 70);
+  ctx.stroke();
+
+
+
+  //rope after hanging
+  ctx.beginPath();
+  ctx.moveTo(150,30);
+  ctx.lineTo(150,50);
+  ctx.stroke();
+
+
+}
+
+function drawBody() {
+//body
+  ctx.beginPath();
+  ctx.moveTo(150,100);
+  ctx.lineTo(150, 200);
+  ctx.stroke();
+
+}
+
+function drawRight() {
+
+  //right leg facing viewer
+  ctx.beginPath();
+  ctx.moveTo(150, 200);
+  ctx.lineTo(125, 275);
+  ctx.stroke();
+
+}
+
+function drawLeft() {
+
+  //left leg facing viewer
+  ctx.beginPath();
+  ctx.moveTo(150, 200);
+  ctx.lineTo(175, 275);
+  ctx.stroke();
+
+}
+
+function drawLeftArm() {
+  //left arm facing viewer
+  ctx.beginPath();
+  ctx.moveTo(150, 125);
+  ctx.lineTo(175, 175);
+  ctx.stroke();
+}
+
+function drawRightArm() {
+  //right arm facing viewer
+  ctx.beginPath();
+  ctx.moveTo(150, 125);
+  ctx.lineTo(125, 175);
+  ctx.stroke();
+
+}
 
 
 
